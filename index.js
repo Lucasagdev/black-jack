e2d63b68ff628d2cfc969634219d34a463bb3711
Lucas Gomes <lucasagostini400@gmail.com>
2024-10-16 22:44:45 -0300

    Adicionando mais um jogador

--- index.js
+++ index.js
@@ -67,3 +67,59 @@ async function quintaCarta() {
     document.getElementById('carta-5').src = imagemCarta
 
 }
+
+let imagemFrente2 = '';
+let mostrandoFrente2 = true;
+
+async function primeiraCarta2() {
+    const baralho = await baralhoDeCartas();
+    const carta = await comprarUmaCarta(baralho.deck_id);
+    imagemFrente2 = carta.cards[0].image;
+
+    const cartaElemento = document.getElementById('carta-1-2');
+    
+    cartaElemento.src = imagemFrente2;
+    mostrandoFrente2 = true;
+}
+
+function virarCarta2() {
+    const cartaElemento = document.getElementById('carta-1-2');
+    const versoCarta = './verso-carta.png';
+
+    if (mostrandoFrente2) {
+        cartaElemento.src = versoCarta;
+        mostrandoFrente2 = false;
+    } else {
+        cartaElemento.src = imagemFrente2;
+        mostrandoFrente2 = true;
+    }
+}
+
+async function segundaCarta2() {
+    const baralho = await baralhoDeCartas()
+    const carta = await comprarUmaCarta(baralho.deck_id)
+    const imagemCarta = carta.cards[0].image
+    document.getElementById('carta-2-2').src = imagemCarta
+
+}
+async function terceiraCarta2() {
+    const baralho = await baralhoDeCartas()
+    const carta = await comprarUmaCarta(baralho.deck_id)
+    const imagemCarta = carta.cards[0].image
+    document.getElementById('carta-3-2').src = imagemCarta
+
+}
+async function quartaCarta2() {
+    const baralho = await baralhoDeCartas()
+    const carta = await comprarUmaCarta(baralho.deck_id)
+    const imagemCarta = carta.cards[0].image
+    document.getElementById('carta-4-2').src = imagemCarta
+
+}
+async function quintaCarta2() {
+    const baralho = await baralhoDeCartas()
+    const carta = await comprarUmaCarta(baralho.deck_id)
+    const imagemCarta = carta.cards[0].image
+    document.getElementById('carta-5-2').src = imagemCarta
+
+}
